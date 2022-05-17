204d90abed9baee92970aad047f1c86ddf4c3c92
bayulalu <bayulalu3@gmail.com>
2022-05-17 12:15:42 +0800

    udangan

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Wedding &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+
+
+	<div id="fh5co-couple">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center fh5co-heading animate-box">
+					<h2>Syukuran dan Akikah!</h2>
+					<!-- <h3>November 28th, 2016 New York, USA</h3> -->
+					<!-- <p>We invited you to celebrate our wedding</p> -->
+				</div>
+			</div>
+			<h3 class="text-center ass">Assalamu'alakum Wr.Wb.</h3>
+
+			<div class="couple-wrap animate-box">
+				<div class="couple-half">
+					<!-- <div class="groom">
+						<img src="images/groom.jpg" alt="groom" class="img-responsive">
+						
+					</div> -->
+					<div class="desc-groom">
+
+						<!-- <h3>Lalu Muahammad Al Fatih Kahyrullah Alami</h3> -->
+						<p> 
+							Dengan Memanjatkan puji syukur kepada Allah Swt, Atas segala pertolongan telah lahir putra kami yang ke-1 pada hari kamis 13 April 2022. 
+							Dengan Besar Hati Kami Beri nama: <b> Lalu Muhammad Al Fatih Khairullah Alami </b>
+					</p>
+						<img src="images/IMG20220413134226.jpg" width="200" alt="" class="img-rounded">
+						<!-- <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove</p> -->
+					</div>
+				</div>
+				<!-- <p class="heart text-center"><i class="icon-heart2"></i></p> -->
+				<div class="couple-half">
+					<!-- <div class="bride">
+						<img src="images/bride.jpg" alt="groom" class="img-responsive">
+					</div> -->
+					<div class="desc-bride">
+						<p> kami sekeluarga bermaksud Mengundang Bapak/Ibu/Sdr/i&nbsp; untuk berkenan hadir dalam acara Tasyakuran Aqiqah putra pertama kami :</p>
+						<ul>
+							<li>Hari/Tanggal : Kamis, 26 Mei 2022  </li>
+							<li>Pukul : 11.00 WITA  </li>
+							<li>Alamat : <a href="https://goo.gl/maps/18xS1zk3uPRnCAsk8" target="_blank"> Belakang SD 1 Kalianyar Kec Terara Lotim </a> </li>
+
+						</ul>
+				
+					</div>
+				</div>
+			</div>
+		
+		</div>
+		<br>
+		<p class="text-center">Merupakan suatu kehormatan bagi kami sekeluarga apabila Bapak/Ibu berkenan <br> hadir untuk bersama salam acara tersebut</p>
+			<p class="text-center">Teriamakasih Atas kehadirannya dan doanya kami ucapakan terimakasih</p>
+			<!-- <img src="images/ihr_qr_code_ohne_logo.jpg" alt=""> -->
+	</div>
+	<!-- <div class="container">
+
+		<div  class="simply-countdown simply-countdown-one"></div>
+
+	</div> -->
+
+	<div id="fh5co-counter" class="fh5co-bg fh5co-counter" style="background-image:url(images/img_bg_5.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+			<div class="display-t">
+					<div class="display-tc">
+		<div  class="simply-countdown simply-countdown-one"></div>
+					</div></div>
+				
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+
+		<iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15778.137484503868!2d116.39836146977538!3d-8.640618699999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x56527000cc42e13f!2sSDN%201%20KALIANYAR!5e0!3m2!1sid!2sid!4v1652756454080!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+	
+	
+	
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	<script>
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+		year: 2022, 
+		month: 5, 
+		day: 26,
+		hours:11.00,
+		minutes:0,
+		seconds:0,
+		pluralLetter:0
+
+    });
+
+</script>
+
+	</body>
+</html>
+
